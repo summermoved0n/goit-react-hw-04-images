2fae8eada46c4dcbddeaae1fc3e75c03a903c846
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-01-06 10:25:31 -0500

    deleted old app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import PixabayApiService from './helpers/helper-api';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
@@ -63,7 +63,6 @@ export default function App() {
     <div className={css.App}>
       {showModal && <Modal closeModal={closeModal} modalImage={modalImage} />}
       <Searchbar onSubmit={searchForm} />
-
       {images.length > 0 && <ImageGallery getId={getImageId} images={images} />}
       {status === 'pending' && (
         <div className={css.Loader}>
@@ -79,108 +78,4 @@ export default function App() {
       )}
     </div>
   );
-}
-
-class oldApp extends Component {
-  state = {
-    searchQuery: '',
-    images: [],
-    page: 1,
-    isMoreImages: false,
-    randomId: 0,
-    // error: null,
-    status: 'idle',
-    showModal: false,
-    modalImage: null,
-  };
-
-  async componentDidUpdate(_, prevState) {
-    const { searchQuery, page, randomId } = this.state;
-    if (
-      prevState.searchQuery !== searchQuery ||
-      prevState.page !== page ||
-      prevState.randomId !== randomId
-    ) {
-      try {
-        this.setState({
-          status: 'pending',
-        });
-
-        const { hits, totalHits } = await pixabayAPI.getPixabayData(
-          searchQuery,
-          page
-        );
-
-        this.setState(prevState => ({
-          images: [...prevState.images, ...hits],
-          isMoreImages: page < Math.ceil(totalHits / 12),
-          status: 'resolved',
-        }));
-      } catch (error) {
-        console.log(error);
-        this.setState({
-          // error: error.message,
-          status: 'rejected',
-        });
-      }
-    }
-  }
-
-  searchForm = searchQuery => {
-    this.setState({
-      searchQuery,
-      images: [],
-      page: 1,
-      isMoreImages: false,
-      randomId: Math.random(),
-    });
-  };
-
-  handleLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
-
-  closeModal = () => {
-    this.setState({
-      showModal: false,
-    });
-  };
-
-  getImageId = id => {
-    const modalImage = this.state.images.find(image => image.id === id);
-    this.setState({ showModal: true, modalImage });
-  };
-
-  render() {
-    const { searchQuery, images, isMoreImages, status, showModal, modalImage } =
-      this.state;
-    console.log('modalImage: ', modalImage);
-    console.log(images);
-
-    return (
-      <div className={css.App}>
-        {showModal && (
-          <Modal closeModal={this.closeModal} modalImage={modalImage} />
-        )}
-        <Searchbar onSubmit={this.searchForm} />
-        {status === 'pending' && (
-          <div className={css.Loader}>
-            <Loader />
-          </div>
-        )}
-        {images.length > 0 && (
-          <ImageGallery getId={this.getImageId} images={images} />
-        )}
-        {isMoreImages && <Button onLoad={this.handleLoadMore} />}
-        {status === 'rejected' && <p>Sorry. Something went wrong! 😥</p>}
-        {status === 'resolved' && images.length === 0 && (
-          <p>
-            Sorry. <b>'{searchQuery}'</b> not found...
-          </p>
-        )}
-      </div>
-    );
-  }
 }
